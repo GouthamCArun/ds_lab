2a36680b81a656744deec865a288ee9d7156ad72
GOUTHAM C ARUN <97888691+GouthamCArun@users.noreply.github.com>
2022-12-05 12:39:45 +0530

    Update que_link.c

--- ds assignment/que_link.c
+++ ds assignment/que_link.c
@@ -3,24 +3,24 @@
 
 struct node{
 	int data;
-	stuct node *next;
+	struct node *next;
 	};
 	
-struct node front=NULL;
-struct node rear=NULL;
+struct node* front=NULL;
+struct node* rear=NULL;
 
 void enque(int value)
 {
-	struct node *Newnode=(struct node*)malloc(sizeof(struct node);
+	struct node *Newnode=(struct node*)malloc(sizeof(struct node));
 	if (front==NULL && rear==NULL)
 	{
-		Newnode.data=value;
+		Newnode->data=value;
 		front=rear=Newnode;
 		
 	}
 	else
 	{
-		Newnode.data=value;
+		Newnode->data=value;
 		rear=Newnode;
 	
 	}
@@ -31,5 +31,48 @@ void deque()
 	front=front->next;
 
 }
-
+void display()
+{
+	struct node *temp;
+	temp=front;
+	while (temp!=NULL)
+	{
+		printf("%d",temp->data);
+		temp=temp->next;
+	}
+}
 	
+void main()
+{
+	int ch,c=1;
+	while (c==1)
+	{
+	printf("1.push\n");
+	printf("2.pop\n");
+	printf("3.display\n");
+	printf("enter choice;");
+	scanf("%d",&ch);
+	if (ch==1)
+	{
+		int val;
+		printf("enter val;");
+		scanf("%d",&val);
+		enque(val);
+	}
+	else if(ch==2)
+	{
+		deque();
+	}
+	else if(ch==3)
+	{
+		display();
+	}
+	
+	else
+	{
+		printf("invalid\n");
+	}
+	printf("\n do u want to contine;");
+	scanf("%d",&c);
+	}
+}
